b54440f440d6f717e0a6d8ab5c81abab0912653d
Jose Victor Persset <jpersset@gmail.com>
2022-11-13 16:46:06 -0300

    Add media queries + scroll effects in js

--- src/js/main.js
+++ src/js/main.js
@@ -44,3 +44,37 @@ function scrollActive() {
     }
   });
 }
+
+/* Scroll Reveal Animation*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "80px",
+  duration: 2000,
+  reset: true,
+});
+
+/*Scroll Home*/
+sr.reveal(".home_title", {});
+sr.reveal(".home_scroll", { delay: 200 });
+sr.reveal(".home_img", { origin: "right", delay: 400 });
+
+/*Scroll About*/
+sr.reveal(".about_img", { delay: 500 });
+sr.reveal(".about_subtitle", { delay: 300 });
+sr.reveal(".about_profession", { delay: 400 });
+sr.reveal(".about_text", { delay: 500 });
+sr.reveal(".about_social-icon", { delay: 600, interval: 200 });
+
+/*Scroll Skills */
+sr.reveal(".skills_subtitle", {});
+sr.reveal(".skills_name", { distance: "20px", delay: 50, interval: 100 });
+sr.reveal(".skills_img", { delay: 400 });
+
+/*Scroll Portfolio*/
+sr.reveal(".portfolio_img", { interval: 200 });
+
+/*Scroll Contact */
+sr.reveal(".contact_subtitle", {});
+sr.reveal(".contact_text", { interval: 200 });
+sr.reveal(".contact_input", { delay: 400 });
+sr.reveal(".contact_text", { delay: 200 });
