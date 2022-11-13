07a163f4ddecab796188277c06d1b0300ccc80d3
Jose Victor Persset <jpersset@gmail.com>
2022-11-13 14:32:42 -0300

    Add functios related to menu scroll and section selects

--- src/js/main.js
+++ src/js/main.js
@@ -19,3 +19,28 @@ function linkAction() {
   navMenu.classList.remove("show");
 }
 navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+/*==== Scroll Sections Active Link ====*/
+const sections = document.querySelectorAll("section[id]");
+
+window.addEventListener("scroll", scrollActive);
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = curent.offsetTop;
+    sectionId = current.getAttribute("id");
+
+    if (scrooY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelectorAll(".nav_menu a[href*=" + sectionId + "]")
+        .classList.add("active");
+    } else {
+      document
+        .querySelector('.nav_menu a[href*=" + sectionId + "]')
+        .classList.remove("active");
+    }
+  });
+}
